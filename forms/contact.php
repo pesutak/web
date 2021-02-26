14f37a05c7260556d6682fe9e8d04ffb37308ec5
Martin Slavkovsky <xslavkov@gmail.com>
2021-02-26 10:58:26 +0100

    contact us form + Work sans for headers

--- forms/contact.php
+++ forms/contact.php
@@ -19,9 +19,8 @@
   $contact->ajax = true;
   
   $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
   $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
+  $contact->subject = 'Web contact us form';
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
   /*
